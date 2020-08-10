eaf5b65633254a4481ac166e27e585e771ae2f11
Ramon Urchipia <ramonurchipia@gmail.com>
2020-08-10 16:54:34 -0300

    arreglando menu

--- js/main.js
+++ js/main.js
@@ -4,3 +4,8 @@ let navMenu = document.querySelector('.nav__menu');
 botonMenu.addEventListener('click', () => {
   navMenu.classList.toggle('show');
 });
+
+navMenu.addEventListener('click', () => {
+  navMenu.classList.remove('show');
+});
+
